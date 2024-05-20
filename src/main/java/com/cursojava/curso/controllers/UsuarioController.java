c3f2b419412ed71771796caedda460020d9cc365
AleChiarilli <alejandrochiarilli@gmail.com>
2024-05-20 21:31:25 +0200

    table structures

--- src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -1,5 +1,7 @@
 package com.cursojava.curso.controllers;
 
+import com.cursojava.curso.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +10,17 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value = "prueba")
-    public List<String> prueba() {
-        return List.of("Manzana", "Kiwi", "Platano");
+    @RequestMapping(value = "usuario/{id}")
+    public Usuario getUsuario(@PathVariable long id) {
+
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("Ale");
+        usuario.setApellido("Chiari");
+        usuario.setEmail("alejandro@gmail.com");
+        usuario.setTelefono("555555");
+        usuario.setContrasena("123456");
+
+        return usuario;
     }
 }
