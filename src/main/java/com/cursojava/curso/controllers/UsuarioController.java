67f1e1787330cccbdc1541e17b740d46af649789
AleChiarilli <alejandrochiarilli@gmail.com>
2024-05-20 22:25:40 +0200

    forEach table

--- src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,4 +24,41 @@ public class UsuarioController {
 
         return usuario;
     }
+
+    @RequestMapping(value = "usuarios")
+    public List<Usuario> getUsuarios() {
+
+        List<Usuario> usuarios = new ArrayList<>();
+
+        Usuario usuario1 = new Usuario();
+        usuario1.setId(123L);
+        usuario1.setNombre("Ale");
+        usuario1.setApellido("Chiari");
+        usuario1.setEmail("alejandro@gmail.com");
+        usuario1.setTelefono("555555");
+        usuario1.setContrasena("123456");
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setId(456L);
+        usuario2.setNombre("Maria");
+        usuario2.setApellido("Gonzalez");
+        usuario2.setEmail("Maria@gmail.com");
+        usuario2.setTelefono("555555");
+        usuario2.setContrasena("123456");
+
+        Usuario usuario3 = new Usuario();
+        usuario3.setId(789L);
+        usuario3.setNombre("Carlos");
+        usuario3.setApellido("Amaya");
+        usuario3.setEmail("Carlos@gmail.com");
+        usuario3.setTelefono("555555");
+        usuario3.setContrasena("123456");
+
+        usuarios.add(usuario1);
+        usuarios.add(usuario2);
+        usuarios.add(usuario3);
+
+        return usuarios;
+    }
+
 }
